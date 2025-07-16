5e10482046efe6c0242d4e4d9182b30a42f877f2
Wu Baiyang (Student) <bw350@students.waikato.ac.nz>
2025-07-17 01:32:56 +1200

    set one wall

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-var speed = 200  # Movement speed in pixels/second
+var speed = 300  # Movement speed in pixels/second
+var sprint_speed = 600 
 var movement = Vector2.ZERO
 
 func _physics_process(delta):
@@ -15,8 +16,13 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		movement.y += 1
 
+	# Sprint if spacebar is pressed
+	var current_speed = speed
+	if Input.is_action_pressed("sprint"):
+		current_speed = sprint_speed
+
 	# Normalize to prevent faster diagonal movement
-	movement = movement.normalized() * speed
+	movement = movement.normalized() * current_speed
 
 	# Apply movement
 	velocity = movement
